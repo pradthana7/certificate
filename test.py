351ff5a9517922c455e55940480a2546cf1cf299
pradthana7 <pradthana797@gmail.com>
2023-02-16 20:21:25 +0700

    update func

--- test.py
+++ test.py
@@ -1,43 +1,43 @@
-
 import hashlib
 
-
+data_list = []
+index = -1
 
 def sha_512(t):
-    return hash(t)
+    # join str before sha512
+    joinstr = "".join(data_list[-1])
+    print("joinstr>>>", joinstr)
+    hash_data = hashlib.sha512(joinstr.encode())
+    res = hash_data.hexdigest()
+    print("The hexadecimal equivalent of SHA512 is : ",res)
 
-
-t = [["pradthana","b632213","algorithm","2022/02/01",100]]
-
+    with open("Signature{}.txt".format(index),"w") as file:
+        file.write(str(res))
 
 
 while True:
-
-
     sname = input("Enter Name: ")
     sid = input("Enter sid: ")
     pname = input("Enter project name: ")
     exdate = input("date of experied: ")
     sc = input("score: ")
 
+    index += 1
+    # add new dats list to all data list
+    new_data = [[sname,sid,pname,exdate,sc]]
+    # extend  new_date to t: list
+    data_list.extend(new_data)
 
-    new_item = [[sname,sid,pname,exdate,sc]]
+    print("new_data",new_data)
+    print("after append >>>", data_list)
 
+    with open("Certificate.txt", "w") as file:
+        file.write(str(data_list))
+    
+    sha_512(data_list)
 
-    t.extend(new_item)
+    
 
-    print("new_item",new_item)
-    print("old list t>>>",t)
-    print("after append >>>", t)
+    
 
-
-    with open("listFile.txt", "w") as file:
-        file.write(str(t))
-
-
-    # join str before sha512
-    joinstr = "".join(t[-1])
-    print("joinstr>>>", joinstr)
-    res = hashlib.sha512(joinstr.encode())
-    print("The hexadecimal equivalent of SHA512 is : ",res.hexdigest())
 
