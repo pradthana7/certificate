530fe904db3d69685334518ab029dfac859ce185
pradthana7 <pradthana797@gmail.com>
2023-02-17 15:28:31 +0700

    gui

--- GUI.py
+++ GUI.py
@@ -15,11 +15,14 @@ GUI = Tk()
 GUI.title("My Certificate")
 GUI.geometry("1000x700")
 
+FONT30 = ("Angsana New", 30)
+FONT20 = ("Angsana New", 20)
+
 data_list = []
 index = -1
 
-####################func###########################
-def Append():
+####################signature func###########################
+def Append(event=None):
     global index
     index += 1
 
@@ -85,47 +88,81 @@ def Signature(msg):
     # v_result.set(text)
     messagebox.showinfo("success", text)
 
-####################func###########################
+    ###############clear textbox###################
+    v_sname.set("")
+    v_pname.set("")
+    v_exdate.set("")
+    v_sc.set("")
+    v_sid.set("")
+    v_result.set("")
+    ###############fix cursor at E1################
+    E1.focus()
 
-L = Label(GUI,text="Certificate Issuance Program", font=("Angsana New", 30))
+####################GUI func###########################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##############################################################
+
+L = Label(GUI,text="Certificate Issuance Program", font=FONT30)
 L.pack(pady=20)
 
-L = Label(GUI,text="Student Name", font=("Angsana New", 20))
+L = Label(GUI,text="Student Name", font=FONT20)
 L.pack()
 v_sname = StringVar()
-E1 = ttk.Entry(GUI,textvariable=v_sname,font=("Angsana New", 20))
+E1 = ttk.Entry(GUI,textvariable=v_sname,font=FONT20)
 E1.pack(pady=5)
 
-L = Label(GUI,text="Student ID", font=("Angsana New", 20))
+L = Label(GUI,text="Student ID", font=FONT20)
 L.pack()
 v_sid = StringVar()
-E2 = ttk.Entry(GUI,textvariable=v_sid,font=("Angsana New", 20))
+E2 = ttk.Entry(GUI,textvariable=v_sid,font=FONT20)
 E2.pack(pady=5)
 
-L = Label(GUI,text="Project Name", font=("Angsana New", 20))
+L = Label(GUI,text="Project Name", font=FONT20)
 L.pack()
 v_pname = StringVar()
-E3 = ttk.Entry(GUI,textvariable=v_pname,font=("Angsana New", 20))
+E3 = ttk.Entry(GUI,textvariable=v_pname,font=FONT20)
 E3.pack(pady=5)
 
-L = Label(GUI,text="Date Of Experied (YYYY-MM-DD)", font=("Angsana New", 20))
+L = Label(GUI,text="Date Of Experied (YYYY-MM-DD)", font=FONT20)
 L.pack()
 v_exdate = StringVar()
-E4 = ttk.Entry(GUI,textvariable=v_exdate,font=("Angsana New", 20))
+E4 = ttk.Entry(GUI,textvariable=v_exdate,font=FONT20)
 E4.pack(pady=5)
 
-L = Label(GUI,text="Please enter a score between 60 and 100", font=("Angsana New", 20))
+L = Label(GUI,text="Please enter a score between 60 and 100", font=FONT20)
 L.pack()
 v_sc = StringVar()
-E5 = ttk.Entry(GUI,textvariable=v_sc,font=("Angsana New", 20))
+E5 = ttk.Entry(GUI,textvariable=v_sc,font=FONT20)
 E5.pack(pady=5)
+E5.bind("<Return>", Append) #press enter  will be submit
 
 B1 = ttk.Button(GUI,text="submit", command=Append)
-B1.pack(pady=10, ipadx=20, ipady=10)
+B1.pack(pady=30, ipadx=20, ipady=10)
 
 v_result = StringVar()
-result = ttk.Label(GUI,textvariable=v_result, font=("Angsana New", 25), foreground="green")
-result.pack()
+result = ttk.Label(GUI,textvariable=v_result, foreground="green")
+
+
 
 
 GUI.mainloop()
