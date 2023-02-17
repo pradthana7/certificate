06abdc37e3caec193888bc165209856b1e85d80b
pradthana7 <pradthana797@gmail.com>
2023-02-17 23:58:32 +0700

    i'm done

--- GUI.py
+++ GUI.py
@@ -1,5 +1,5 @@
-#code ref--> 1. https://cryptography.io/en/latest/hazmat/primitives/asymmetric/rsa/#key-loading
-#code ref--> 2. Chat GPT
+# code ref--> 1. https://cryptography.io/en/latest/hazmat/primitives/asymmetric/rsa/#key-loading
+# code ref--> 2. Chat GPT
 
 from tkinter import *
 from tkinter import ttk, messagebox
@@ -24,9 +24,9 @@ data_list = []
 index = 0
 
 
-############################config tab#########################
-Tab =ttk.Notebook(GUI)
-Tab.pack(fill=BOTH,expand=1)
+############################ config tab#########################
+Tab = ttk.Notebook(GUI)
+Tab.pack(fill=BOTH, expand=1)
 
 T1 = Frame(Tab)
 T2 = Frame(Tab)
@@ -36,13 +36,11 @@ icon_tab1 = PhotoImage(file="tab1.png")
 icon_tab2 = PhotoImage(file="tab2.png")
 
 
-Tab.add(T1,text="signature",image=icon_tab1,compound="left")
-Tab.add(T2,text="vertify",image=icon_tab2,compound="left")
+Tab.add(T1, text="signature", image=icon_tab1, compound="left")
+Tab.add(T2, text="vertify", image=icon_tab2, compound="left")
 
 
-
-
-####################signature func###########################
+#################### signature func###########################
 
 def Append(event=None):
 
@@ -63,9 +61,8 @@ def Append(event=None):
 
     sc = v_sc.get()
     data_list.append(sc)
-    
-    SHA_512(data_list)
 
+    SHA_512(data_list)
 
 
 def SHA_512(data_list):
@@ -87,11 +84,11 @@ def SHA_512(data_list):
 
 
 def Signature(msg):
-    # Load private key previouly generated 
+    # Load private key previouly generated
     with open('private_key.pem', 'rb') as file:
         private_pem = file.read()
         private_key = load_pem_private_key(private_pem, password=None)
-        print("private_key",private_key)
+        print("private_key", private_key)
 
     # signing
     signature = private_key.sign(
@@ -110,59 +107,60 @@ def Signature(msg):
     # v_result.set(text)
     messagebox.showinfo("success", text)
 
-    ###############clear textbox###################
+    ############### clear textbox###################
     v_sname.set("")
     v_pname.set("")
     v_exdate.set("")
     v_sc.set("")
     v_sid.set("")
     v_result.set("")
-    ###############fix cursor at E1################
+    ############### fix cursor at E1################
     E1.focus()
 
-##############################Tab1 signature################################
+############################## Tab1 signature################################
 
-L = Label(T1,text="Certificate Issuance Program", font=FONT30)
+
+L = Label(T1, text="Certificate Issuance Program", font=FONT30)
 L.pack(pady=20)
 
-L = Label(T1,text="Student Name", font=FONT20)
+L = Label(T1, text="Student Name", font=FONT20)
 L.pack()
 v_sname = StringVar()
-E1 = ttk.Entry(T1,textvariable=v_sname,font=FONT20)
+E1 = ttk.Entry(T1, textvariable=v_sname, font=FONT20)
 E1.pack(pady=5)
 
-L = Label(T1,text="Student ID", font=FONT20)
+L = Label(T1, text="Student ID", font=FONT20)
 L.pack()
 v_sid = StringVar()
-E2 = ttk.Entry(T1,textvariable=v_sid,font=FONT20)
+E2 = ttk.Entry(T1, textvariable=v_sid, font=FONT20)
 E2.pack(pady=5)
 
-L = Label(T1,text="Project Name", font=FONT20)
+L = Label(T1, text="Project Name", font=FONT20)
 L.pack()
 v_pname = StringVar()
-E3 = ttk.Entry(T1,textvariable=v_pname,font=FONT20)
+E3 = ttk.Entry(T1, textvariable=v_pname, font=FONT20)
 E3.pack(pady=5)
 
-L = Label(T1,text="Date Of Experied (YYYY-MM-DD)", font=FONT20)
+L = Label(T1, text="Date Of Experied (YYYY-MM-DD)", font=FONT20)
 L.pack()
 v_exdate = StringVar()
-E4 = ttk.Entry(T1,textvariable=v_exdate,font=FONT20)
+E4 = ttk.Entry(T1, textvariable=v_exdate, font=FONT20)
 E4.pack(pady=5)
 
-L = Label(T1,text="Please enter a score between 60 and 100", font=FONT20)
+L = Label(T1, text="Please enter a score between 60 and 100", font=FONT20)
 L.pack()
 v_sc = StringVar()
-E5 = ttk.Entry(T1,textvariable=v_sc,font=FONT20)
+E5 = ttk.Entry(T1, textvariable=v_sc, font=FONT20)
 E5.pack(pady=5)
-E5.bind("<Return>", Append) #press enter  will be submit
+E5.bind("<Return>", Append)  # press enter  will be submit
 
-B1 = ttk.Button(T1,text="submit", command=Append)
+B1 = ttk.Button(T1, text="submit", command=Append)
 B1.pack(pady=30, ipadx=20, ipady=10)
 
 v_result = StringVar()
-result = ttk.Label(T1,textvariable=v_result, foreground="green")
+result = ttk.Label(T1, textvariable=v_result, foreground="green")
 
-  
+
 ##################################### tab2 verify #############################################
 
 def Verification():
@@ -170,7 +168,6 @@ def Verification():
     with open('public_key.pem', 'rb') as file:
         public_pem = file.read()
         public_key = load_pem_public_key(public_pem)
-        
 
     with open("Certificate{}.txt".format(ver_index), "r") as file:
         list_certificate = file.read().split("\n")
@@ -187,7 +184,7 @@ def Verification():
     if date_of_expired <= today:
         text = "your certificate has been expired"
         messagebox.showerror("expired", text)
-        
+
     else:
 
         try:
@@ -220,23 +217,23 @@ def Verification():
 
     _ver_index.set("")
 
-############################end verify func############################
+############################ end verify func############################
 
-L = Label(T2,text="VERIFY", font=FONT30)
+
+L = Label(T2, text="VERIFY", font=FONT30)
 L.pack(pady=20)
 
-L = Label(T2,text="Please enter the index to verify the signature\n for example \"Certificate0\" = index 0 (enter 0)", font=FONT30)
+L = Label(T2, text="Please enter the index to verify the signature\n for example \"Certificate0\" = index 0 (enter 0)", font=FONT20)
 L.pack(pady=20)
 
 _ver_index = StringVar()
-E3 = ttk.Entry(T2,textvariable=_ver_index,font=FONT20)
+E3 = ttk.Entry(T2, textvariable=_ver_index, font=FONT20)
 E3.pack(pady=20)
 E3.focus()
 
-B2 = ttk.Button(T2,text="verify", command=Verification)
+B2 = ttk.Button(T2, text="verify", command=Verification)
 B2.pack(pady=20, ipadx=15, ipady=10)
 B2.bind("<Return>", Verification)
-
 
 
 GUI.mainloop()
